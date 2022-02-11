00cc4281bbd84afaa2c145843df90cc824fd26e1
Timothy Truty <ttruty@gmail.com>
2022-02-11 08:15:33 -0600

    Update watchconnectivity.js

--- www/watchconnectivity.js
+++ www/watchconnectivity.js
@@ -1,21 +1,24 @@
-var exec = cordova.require('cordova/exec');
+try {
+  var exec = cordova.require('cordova/exec');
 
-//apple watch class
-var AppleWatchConnectivity = function(){};
+  //apple watch class
+  var AppleWatchConnectivity = function(){};
 
-AppleWatchConnectivity.init = function (successCallback, errorCallback) {
-  exec(successCallback, errorCallback, "AppleWatchConnectivity", "init", []);
-};
-
-
-AppleWatchConnectivity.messageReceiver = function (onNewMessageCallback, errorCallback) {
-  exec(onNewMessageCallback, errorCallback, "AppleWatchConnectivity", "messageReceiver", []);
-};
-
-AppleWatchConnectivity.sendMessage = function (message, successCallback, errorCallback) {
-  exec(successCallback, errorCallback, "AppleWatchConnectivity", "sendMessage", [message]);
-};
+  AppleWatchConnectivity.init = function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "AppleWatchConnectivity", "init", []);
+  };
 
 
-module.exports = AppleWatchConnectivity;
+  AppleWatchConnectivity.messageReceiver = function (onNewMessageCallback, errorCallback) {
+    exec(onNewMessageCallback, errorCallback, "AppleWatchConnectivity", "messageReceiver", []);
+  };
 
+  AppleWatchConnectivity.sendMessage = function (message, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "AppleWatchConnectivity", "sendMessage", [message]);
+  };
+
+
+  module.exports = AppleWatchConnectivity;
+} catch (error) {
+  console.error(error);
+}
